4706e4887f06d6df4a217678450b240c1e64ec52
Samuel Birhanu <samibre121@gmail.com>
2024-11-20 10:58:28 +0300

    setting up the backend api tests to run in a dockerized environment

--- back/tests/test_routes.py
+++ back/tests/test_routes.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from fastapi import FastAPI
 
 # Import the router instead of routes module
-from back.src.api.routes import router
+from src.api.routes import router
 
 # Create FastAPI app and include the router
 app = FastAPI()
